9815237dfd46949e2ae50afdd2be3f021200eacb
Jules Chendjou <julestl@yahoo.fr>
2024-02-26 13:52:40 -0500

    update the script file

--- scripts/addvariable.sh
+++ scripts/addvariable.sh
@@ -10,6 +10,7 @@ echo $SYSTEM_TEAMPROJECTID
 echo $Build_DefinitionVersion
 echo $Build_BuildId
 echo $risk
+
 url="$SYSTEM_TEAMFOUNDATIONSERVERURI/$SYSTEM_TEAMPROJECTID/_apis/build/definitions/$Build_DefinitionVersion?api-version=7.1-preview.7"
 echo "Definition URL: $url"
 
