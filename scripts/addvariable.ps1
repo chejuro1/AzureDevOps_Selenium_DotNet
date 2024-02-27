7993a24642e3f73464f43445441b97d8eec74bc1
Jules Chendjou <julestl@yahoo.fr>
2024-02-27 17:11:51 -0500

    add powershel task

--- scripts/addvariable.ps1
+++ scripts/addvariable.ps1
@@ -9,40 +9,30 @@ Write-Host "SYSTEM_TEAMPROJECTID: $SYSTEM_TEAMPROJECTID"
 
 # Get the variable group ID for 'Risk_url'
 $url = ('{0}{1}/_apis/distributedtask/variablegroups?api-version=7.1-preview.2' -f $SYSTEM_TEAMFOUNDATIONSERVERURI, $SYSTEM_TEAMPROJECTID)
-
-# Get the pipeline definition
-$pipeline = Invoke-RestMethod -Uri $url -Method Get -Headers @{
-                  Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"
-              }
-Write-Host "Pipeline = $($pipeline | ConvertTo-Json -Depth 100)"
-Write-Host($pipeline)
-# Update the assigned_to variable with the user's ameuniqueName
-# $pipeline.variables.assigned_to.value = "702"
-
 $response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $SYSTEM_ACCESSTOKEN"}
 $group_id = ($response.value | Where-Object { $_.name -eq "Risk_url" }).id
-Write-Host($group_id)
+
 if (-not $group_id) {
     Write-Host "Failed to get the ID of the 'Risk_url' variable group."
     exit 1
 }
 
 # Update the variable 'risk_url1' in the 'Risk_url' variable group
-$new_value = "https://dev.azure.com/cheindjou/poc/_build/results?buildId=215&view=logs"
-
-# Construct the JSON payload for the update
-$json_payload = @{
-    variables = @{
-        risk_url1 = @{
-            isSecret = $false
-            value = $new_value
+$json = '{
+    "id": ' + $group_id + ',
+    "type": "Vsts",
+    "name": "Risk_url",
+    "variables": {
+        "risk_url1": {
+            "isSecret": false,
+            "value": "https://dev.azure.com/cheindjou/poc/_build/results?buildId=215&view=logs"
         }
     }
-} | ConvertTo-Json
+}'
 
-Write-Host "Updating variable 'risk_url1' with value: $new_value"
-Write-Host "JSON Payload: $json_payload"
+Write-Host "Updating variable 'risk_url1' with value: https://dev.azure.com/cheindjou/poc/_build/results?buildId=215&view=logs"
+Write-Host "JSON Payload: $json"
 
 # Make the PATCH request to update the variable group
 $patchUrl = "${SYSTEM_TEAMFOUNDATIONSERVERURI}${SYSTEM_TEAMPROJECTID}/_apis/distributedtask/variablegroups/${group_id}?api-version=7.1-preview.2"
-Invoke-RestMethod -Uri $patchUrl -Method POST -Body $json_payload -ContentType "application/json" -Headers @{Authorization = "Bearer $SYSTEM_ACCESSTOKEN"}
+Invoke-RestMethod -Uri $patchUrl -Method Put -Body $json -ContentType "application/json" -Headers @{Authorization = "Bearer $SYSTEM_ACCESSTOKEN"}
