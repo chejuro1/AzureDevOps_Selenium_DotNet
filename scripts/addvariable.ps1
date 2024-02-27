32c7c859a48499193494396454b3c4fc975388f5
Jules Chendjou <julestl@yahoo.fr>
2024-02-27 12:18:45 -0500

    add powershel task

--- scripts/addvariable.ps1
+++ scripts/addvariable.ps1
@@ -1,13 +1,15 @@
-$SYSTEM_TEAMFOUNDATIONSERVERURI="$1"
-$SYSTEM_TEAMPROJECTID="$2"
-
+param (
+    [string]$SYSTEM_TEAMFOUNDATIONSERVERURI,
+    [string]$SYSTEM_TEAMPROJECTID,
+    [string]$SYSTEM_ACCESSTOKEN
+)
 
 Write-Host "SYSTEM_TEAMFOUNDATIONSERVERURI: $SYSTEM_TEAMFOUNDATIONSERVERURI"
 Write-Host "SYSTEM_TEAMPROJECTID: $SYSTEM_TEAMPROJECTID"
 
-
 # Get the variable group ID for 'Risk_url'
-$response = Invoke-RestMethod -Uri "$SYSTEM_TEAMFOUNDATIONSERVERURI$SYSTEM_TEAMPROJECTID/_apis/distributedtask/variablegroups?api-version=7.1-preview.2" -Headers @{Authorization = "Bearer $SYSTEM_ACCESSTOKEN"}
+$url = ('{0}{1}/_apis/distributedtask/variablegroups?api-version=7.1-preview.2' -f $SYSTEM_TEAMFOUNDATIONSERVERURI, $SYSTEM_TEAMPROJECTID)
+$response = Invoke-RestMethod -Uri $url -Headers @{Authorization = "Bearer $SYSTEM_ACCESSTOKEN"}
 $group_id = ($response.value | Where-Object { $_.name -eq "Risk_url" }).id
 
 if (-not $group_id) {
@@ -35,6 +37,6 @@ Write-Host "Updating variable 'risk_url1' with value: $new_value"
 Write-Host "JSON Payload: $json_payload"
 
 # Make the PUT request to update the variable group
-Invoke-RestMethod -Uri "${SYSTEM_TEAMFOUNDATIONSERVERURI}${SYSTEM_TEAMPROJECTID}/_apis/distributedtask/variablegroups/${group_id}?api-version=7.1-preview.2" -Method Put -Body $json -ContentType "application/json" -Headers @{
-                  Authorization = "Bearer $env:SYSTEM_ACCESSTOKEN"
+Invoke-RestMethod -Uri "${SYSTEM_TEAMFOUNDATIONSERVERURI}${SYSTEM_TEAMPROJECTID}/_apis/distributedtask/variablegroups/${group_id}?api-version=7.1-preview.2" -Method Put -Body $json_payload -ContentType "application/json" -Headers @{
+                  Authorization = "Bearer $SYSTEM_ACCESSTOKEN"
                 }
