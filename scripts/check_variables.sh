a0e03d1386294e453ef7f7cc9667389357b13779
Jules Chendjou <julestl@yahoo.fr>
2024-03-07 10:19:09 -0500

    test check variable

--- scripts/check_variables.sh
+++ scripts/check_variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check if arguments are provided
-if [ $# -eq 0 ]; then
-  echo "Error: No variables provided. Please provide at least one variable name as an argument."
-  exit 1
-fi
+# if [ $# -eq 0 ]; then
+#   echo "Error: No variables provided. Please provide at least one variable name as an argument."
+#   exit 1
+# fi
 
 # Loop through all arguments
 for variable_name in "$@"
@@ -16,5 +16,5 @@ do
   else
     echo "Variable '$variable_name' has a value: ${!variable_name}"
   fi
-  
+
 done
